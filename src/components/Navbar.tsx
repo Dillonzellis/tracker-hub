69b78a770f455414476b3ca12946baac0bbf5ee1
dillonzellis <dillonzellis@gmail.com>
2024-01-19 21:56:07 -0500

    chore: remove unused imports

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,11 @@
-import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
-import { buttonVariants } from "./ui/button";
 
 export const Navbar = () => {
   return (
     <MaxWidthWrapper>
-      <nav className="py-4 space-x-4 text-white">
+      <nav className="space-x-4 py-4 text-white">
         <Button asChild variant="link" className="text-white">
           <Link href="/">Tracker</Link>
         </Button>
